0372129f8cca529b5abed6b292bc2d3def1e3472
Christopher Briddock <christopherbriddock@outlook.com>
2023-07-05 13:08:33 +0100

    ServiceCollection and WebHostBuilder extensions initial commit.

---
+++ src/ChristopherBriddock.AspNetCore.Extensions/WebHostBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+namespace ChristopherBriddock.IdentityServer.Extensions;
+
+public static class WebHostBuilderExtensions
+{
+    /// <summary>
+    /// Adds Kestrel, and uses HTTP/3. Is backward compatible with HTTP/2 and HTTP/1
+    /// </summary>
+    /// <param name="webHostBuilder"></param>
+    public static void AddKestrelConfiguration(this IWebHostBuilder webHostBuilder)
+    {
+        webHostBuilder.ConfigureKestrel((context, options) =>
+        {
+            options.ListenAnyIP(7181, listenOptions =>
+            {
+                listenOptions.Protocols = HttpProtocols.Http1AndHttp2AndHttp3;
+                listenOptions.UseHttps();
+            });
+        });
+    }
+}
---
+++ src/README.md
@@ -0,0 +1,1 @@
+# ChristopherBriddock.AspNetCore.Extensions
